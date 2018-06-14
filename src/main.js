f18d918d8bb051dce9b34095bd0534a87a72c7c9
Юркевич Алексей <yurkevich.a@wildberries.ru>
2018-06-14 10:55:14 +0300

    +

--- src/main.js
+++ src/main.js
@@ -1,8 +1,53 @@
 import Vue from 'vue';
 import './style.scss';
+import genres from './util/genres';
+
 new Vue({
     el:'#app',
-    data:{
-        msg: 'Hello!'
+    components:{
+        'movie-list':{
+            template: `<div id="movie-list">
+             <div v-for="movie in movies" class="movie">{{movie.title}}</div>
+            </div>`,
+            data(){
+                return {
+                    movies:[{
+                        title:'Pop1'
+                    },
+                    {
+                        title:'Pop2'
+                    }]  
+                }
+            }
+
+        },
+        'movie-filter':{
+            data(){
+                return {
+                    genres
+                }
+            },
+            template: `<div id="movie-filter">
+            <h2>FILTER</h2>
+             <div class="filter-group">
+                 <check-filter v-for="genre in genres" :title="genre"></check-filter>
+             </div>
+            </div>`,
+            components:{  
+                'check-filter':{
+                    data(){
+
+                    },
+                    props:[
+                        'title'
+                    ],
+
+                    template:`<div class="check-filter">
+                    <span class="checkbox"></span>
+                    <span class="check-filter-title">{{title}}</span>
+                    </div>`
+                }
+            }
+        }
     }
 })
